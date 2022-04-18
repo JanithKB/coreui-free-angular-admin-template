cda7e053d763ed00e2c4f63ec2d5abadab3af0b7
ouslomis <ouslomis@ou.ac.lk>
2022-04-18 15:44:46 +0530

    gdf

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -2,7 +2,7 @@ import { INavData } from '@coreui/angular';
 
 export const navItems: INavData[] = [
   {
-    name: 'Dashboard',
+    name: 'dfs',
     url: '/dashboard',
     iconComponent: { name: 'cil-speedometer' },
     badge: {
